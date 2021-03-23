f6b1b9dafbd01be96cd1e5044bdbd435ba3135b4
dannebicque <david.annebicque@gmail.com>
2021-03-23 16:28:39 +0100

    Fournisseur

---
+++ src/components/Fournisseur.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="fournisseur in fournisseurs" :key="fournisseur.nom">
+        {{ fournisseur.nom }} ({{ fournisseur.ville}})
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Fournisseur",
+  data() {
+    return {
+      fournisseurs: [
+      { nom: 'Foo', ville:'Troyes' },
+      { nom: 'Bar', ville:'Paris' }
+    ]}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
